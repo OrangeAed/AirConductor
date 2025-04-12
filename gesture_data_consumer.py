0a8198a415c9c732f149ae1e68ddd18a61aad19a
OrangeAed <ajsimonsrudolph@gmail.com>
2025-04-12 17:20:34 -0400

    Gui now has video stream, not sending anything though

--- gesture_data_consumer.py
+++ gesture_data_consumer.py
@@ -1,11 +1,14 @@
+import datetime
+
 import aio_pika
 import asyncio
 import json
-
+from gesture_data_publisher import GestureDataPublisher
+import threading
 
 def on_message(message: aio_pika.IncomingMessage):
     gesture_data = json.loads(message.body.decode())
-    print("Received", gesture_data["gesture"])
+    print("Received", gesture_data["timestamp"])
 
 
 async def main():
@@ -22,6 +25,18 @@ async def main():
     while True:
         await asyncio.sleep(0.5)
 
+def run_loop(gdp):
+    asyncio.run(gdp.send_gesture_data())
+
+def set_message():
+    while True:
+        gdp.set_gesture_data({"timestamp": datetime.datetime.now()})
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    gdp = GestureDataPublisher('gesture_queue')
+    gdp.set_gesture_data({"timestamp": "whatever"})
+    t1 = threading.Thread(target=run_loop(gdp))
+    t2 = threading.Thread(target=set_message)
+    t1.start()
+    t2.start()
+    t1.join()
--- gesture_data_publisher.py
+++ gesture_data_publisher.py
@@ -4,10 +4,13 @@ import json
 import time
 
 class GestureDataPublisher:
-    def __init__(self):
+    def __init__(self, queue_name="gesture_queue"):
         self.gesture_data = {}
+        self.queue_name = queue_name
+        self.send_data = True
 
     def set_gesture_data(self, gesture_data):
+        self.send_data = True
         self.gesture_data = gesture_data
 
     async def send_gesture_data(self):
@@ -15,16 +18,18 @@ class GestureDataPublisher:
         async with connection:
             channel = await connection.channel()
 
-            await channel.declare_queue("gesture_queue", durable=True)
+            await channel.declare_queue(self.queue_name, durable=True)
 
             while True:
-                gesture_data = self.gesture_data
-                message = aio_pika.Message(
-                    body=json.dumps(gesture_data).encode(),
-                    delivery_mode=aio_pika.DeliveryMode.PERSISTENT
-                )
-                await channel.default_exchange.publish(message, routing_key="gesture_queue")
-                print("Sent:", gesture_data)
+                if self.send_data:
+                    self.send_data = False
+                    gesture_data = self.gesture_data
+                    message = aio_pika.Message(
+                        body=json.dumps(gesture_data).encode(),
+                        delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+                    )
+                    await channel.default_exchange.publish(message, routing_key=self.queue_name)
+                    print("Sent:", gesture_data)
                 await asyncio.sleep(1)
 
 if __name__ == "__main__":
