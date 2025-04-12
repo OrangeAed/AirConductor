6db0bf0d0a84281930f2ec50cbcee958af61583e
OrangeAed <ajsimonsrudolph@gmail.com>
2025-04-12 16:09:12 -0400

    updating gdp

--- gesture_data_publisher.py
+++ gesture_data_publisher.py
@@ -3,25 +3,29 @@ import aio_pika
 import json
 import time
 
-async def send_gesture_data():
-    connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
-    async with connection:
-        channel = await connection.channel()
+class GestureDataPublisher:
+    def __init__(self):
+        self.gesture_data = {}
 
-        await channel.declare_queue("gesture_queue", durable=True)
 
-        while True:
-            gesture_data = {
-                "timestamp": time.strftime('%Y-%m-%dT%H:%M:%S'),
-                "gesture" : "swipe_right",
-            }
-            message = aio_pika.Message(
-                body=json.dumps(gesture_data).encode(),
-                delivery_mode=aio_pika.DeliveryMode.PERSISTENT
-            )
-            await channel.default_exchange.publish(message, routing_key="gesture_queue")
-            print("Sent:", gesture_data)
-            await asyncio.sleep(1)
+    async def send_gesture_data(self):
+        connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+        async with connection:
+            channel = await connection.channel()
+
+            await channel.declare_queue("gesture_queue", durable=True)
+
+            while True:
+                gesture_data = self.gesture_data
+                message = aio_pika.Message(
+                    body=json.dumps(gesture_data).encode(),
+                    delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+                )
+                await channel.default_exchange.publish(message, routing_key="gesture_queue")
+                print("Sent:", gesture_data)
+                await asyncio.sleep(1)
 
 if __name__ == "__main__":
-    asyncio.run(send_gesture_data())
+    gdp = GestureDataPublisher()
+    gdp.gesture_data = {"timestamp": "whatever"}
+    asyncio.run(gdp.send_gesture_data())
