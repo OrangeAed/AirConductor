81dd6831d967d294d1dac09c14ce76812814b4a6
jclmb7 <155386759+jclmb7@users.noreply.github.com>
2025-04-12 13:29:38 -0400

    Live stream working, quit with q

---
+++ demo.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+
+    if not success:
+        break
+
+    # Flip the frame horizontally (so it looks like a mirror)
+    frame = cv2.flip(frame, 1)
+
+    cv2.imshow("Live Stream", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
