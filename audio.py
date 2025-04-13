040f4cb1f776f8947df4af141d3cc14f387b3344
Wyatt Mallon <mallonwm@appstate.edu>
2025-04-12 22:53:19 -0400

    Changing for orchestrator

--- audio.py
+++ audio.py
@@ -1,10 +1,6 @@
 import wave
 import pyaudio
 import numpy as np
-import aio_pika
-import asyncio
-import json
-from numpy.lib.function_base import vectorize
 
 SPEED = [(1, 2), (3, 4), (1, 1), (3, 2), (2, 1)]
 VOLUME = [0, 0.25, 0.5, 0.75, 1]
@@ -17,7 +13,9 @@ TRACK_NUMBERS = {
 
 
 class AudioPlayer:
-    def __init__(self, path: str):
+    def __init__(self, path: str, queue):
+        self.queue = queue
+
         ul = wave.open(path + '\\ul.wav', 'rb')
         ur = wave.open(path + '\\ur.wav', 'rb')
         ll = wave.open(path + '\\ll.wav', 'rb')
@@ -37,7 +35,7 @@ class AudioPlayer:
         self.playing = True
         self.speed = 2
 
-    async def play(self, data):
+    def play(self, data):
         data = self._transform_audio(data)
         self.audio_stream.write(data.tobytes())
 
@@ -53,8 +51,7 @@ class AudioPlayer:
         arr = arr[::SPEED[self.speed][0]]
         return np.clip(np.sum(arr, axis=1), -32768, 32767).astype(np.int16)
 
-    def update(self, message: aio_pika.IncomingMessage):
-        data = json.loads(message.body.decode())
+    def update(self, data: dict):
         if data['track'] == 'c':
             self.volumes = np.where((0 <= self.volumes + data['volume']) & (self.volumes + data['volume'] <= 4),
                                     self.volumes + data['volume'],
@@ -69,29 +66,17 @@ class AudioPlayer:
         print(
             f'Track: {data["track"]}\tVolume: {self.get_volume()}\tSpeed: {SPEED[self.speed]}\tPlaying: {self.playing}')
 
+    def run(self):
+        data = self.tracks[:2048]
+        counter = 0
+        while data is not None:
+            if not self.queue.empty():
+                self.update(self.queue.get())
+            if self.playing:
+                self.play(data)
+                counter += 1
+                data = self.tracks[counter * 2048:(counter + 1) * 2048]
 
-async def main(path):
-    connection = await aio_pika.connect_robust('amqp://guest:guest@localhost/')
-    channel = await connection.channel()
-    await channel.queue_delete('gesture_queue')
-    queue = await channel.declare_queue('gesture_queue', durable=True)
-
-    player = AudioPlayer(path)
-
-    data = player.tracks[:2048]
-    counter = 0
-    while data is not None:
-        await queue.consume(player.update)
-        if player.playing:
-            await player.play(data)
-            counter += 1
-            data = player.tracks[counter * 2048:(counter + 1) * 2048]
-
-    player.audio_stream.stop_stream()
-    player.audio_stream.close()
-    player.pyaudio.terminate()
-    await connection.close()
-
-
-if __name__ == '__main__':
-    asyncio.run(main('songs\\Rick Astley - Never Gonna Give You Up'))
+        self.audio_stream.stop_stream()
+        self.audio_stream.close()
+        self.pyaudio.terminate()
