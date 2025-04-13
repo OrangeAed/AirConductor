7bf14cbc3342d8c760072a8a96e95ebc5979bede
OrangeAed <ajsimonsrudolph@gmail.com>
2025-04-12 22:53:59 -0400

    fixing the fake_demo.py

--- AllTogetherNow/fake_demo.py
+++ AllTogetherNow/fake_demo.py
@@ -9,7 +9,7 @@ class FakeDemo:
         while True:
             sleep(1)
             print(f"FakeDemo {self.name} is running...")
-            self.results["speed"] += 1
-            self.results["volume"] += 1
+            self.results["speed"] = 1
+            self.results["volume"] = 1
             self.results["playing"] = not self.results["playing"]
             print(self.results)
